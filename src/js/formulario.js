7b5ffe25c854b50fcdaec8e411765c8b2cec10f3
Cris Plaza <crisika@MacBook-Pro-de-Cris-Plaza.local>
2026-06-14 02:56:19 -0400

    feat: agrega toda la estructura base del proyecto

---
+++ src/js/formulario.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Módulo de formularios listo.");
+    // Aquí implementaremos las reglas de validación (mayoría de edad, correos @inacap.cl, etc.)
+});
---
+++ src/js/productos.js
@@ -0,0 +1,6 @@
+// Arreglo base para renderizar productos dinámicamente
+const productos = []; 
+
+console.log("Módulo de productos cargado.");
+
+// La lógica de fetch a data.json vendrá aquí después
